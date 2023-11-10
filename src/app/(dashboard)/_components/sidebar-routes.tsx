393875f49ec08feea7142b43f9bb71de788d9ddd
= <rohitbhatwara1@gmail.com>
2023-11-10 17:07:04 +0530

    feat(dynamic-routes): create dynamic routes based on teacher or guest

--- src/app/(dashboard)/_components/sidebar-routes.tsx
+++ src/app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,7 @@
 'use client'
 
-import { CompassIcon, LayoutIcon } from 'lucide-react'
+import { BarChartIcon, CompassIcon, LayoutIcon, ListIcon } from 'lucide-react'
+import { usePathname } from 'next/navigation'
 import SidebarItem from './sidebar-item'
 
 const guestRoutes = [
@@ -16,8 +17,24 @@ const guestRoutes = [
   },
 ]
 
+const teacherRoutes = [
+  {
+    icon: ListIcon,
+    label: 'Courses',
+    href: '/teacher/courses',
+  },
+  {
+    icon: BarChartIcon,
+    label: 'Analytics',
+    href: '/teacher/analytics',
+  },
+]
+
 export default function SidebarRoutes() {
-  const routes = guestRoutes
+  const pathname = usePathname()
+  const isTeacherPage = pathname?.includes('/teacher')
+
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes
 
   return (
     <div className="flex w-full flex-col">
