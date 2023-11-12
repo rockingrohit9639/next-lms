42c99415a61432aa16755a0ac44f8d3de705cd50
= <rohitbhatwara1@gmail.com>
2023-11-12 21:49:34 +0530

    feat(dashboard): create dashboard for teacher

---
+++ src/app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import { getAnalytics } from '@/actions/get-analytics'
+import DataCard from './_components/data-card'
+import Chart from './_components/chart'
+
+export default async function Analytics() {
+  const { userId } = auth()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId)
+
+  return (
+    <div className="p-6">
+      <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+
+      <Chart data={data} />
+    </div>
+  )
+}
